893426bcab8f6c0bd82fa8e9ec98c6b5703aace1
Wania <107751144+wania-dev@users.noreply.github.com>
2022-07-28 23:18:59 +0500

    Update main.js

--- business-website/main.js
+++ business-website/main.js
@@ -8,21 +8,20 @@ function onClick(element) {
   
   
   // Toggle between showing and hiding the sidebar when clicking the menu icon
-  var mySidebar = document.getElementById("mySidebar");
-  
-  function w3_open() {
-    if (mySidebar.style.display === 'block') {
-      mySidebar.style.display = 'none';
-    } else {
-      mySidebar.style.display = 'block';
-    }
-  }
-  
-  // Close the sidebar with the close button
-  function w3_close() {
-      mySidebar.style.display = "none";
-  }
+var mySidebar = document.getElementById("mySidebar");
 
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
 //   scroll smooth for browsers that dont support it
 $(document).ready(function(){
     // Add smooth scrolling to all links
